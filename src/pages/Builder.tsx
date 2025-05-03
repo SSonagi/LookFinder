6e2bf3df29445b1ba8dfa2965b6b5427ea8066c8
Sangwook Lee <leesangwook4517@gmail.com>
2025-05-03 17:57:26 -0400

    refactor: restructure app with routing and add new pages

---
+++ src/pages/Builder.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Builder = () => {
+    return (
+        <div>
+            <h1>Builder Page</h1>
+        </div>
+    );
+};
+
+export default Builder;
---
+++ src/pages/Outfits.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Outfits = () => {
+    return (
+        <div>
+            <h1>Outfits Page</h1>
+        </div>
+    );
+};
+
+export default Outfits;
