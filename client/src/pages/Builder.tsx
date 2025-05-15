b5a641fc9e07b962d626c09e5a220186b790b66a
Sangwook Lee <leesangwook4517@gmail.com>
2025-05-15 15:33:04 -0400

    chage for adding vite and tailwind

--- client/src/pages/Builder.tsx
+++ client/src/pages/Builder.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import Header from '../components/header';
 import '../../styles/layout.css';
 
 const Builder = () => {
     return (
-        <div className='page'>
-            <Header/>
-            <div className='builder'>
-                <h1>
-                    Outfit Builder
-                </h1>
-                <div className='main'>
-                    <div className='left'>
-                        
-                    </div>
-                    <div className='right'>
-                        <div className=''>
-                        
-                        </div>
+        <div className='builder'>
+            <h1>
+                Outfit Builder
+            </h1>
+            <div className='main'>
+                <div className='left'>
+                    
+                </div>
+                <div className='right'>
+                    <div className=''>
+                    
                     </div>
                 </div>
             </div>
