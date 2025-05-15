74f6765c1d1ccf0f2b07df23a6305c780f99a72e
tonyy-87 <anthony12871287@yahoo.ca>
2025-05-15 13:46:22 -0400

    deleting react files and trying to add my own files as a place holder to publish on site

---
+++ src/components/Hompage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Look Finder</h1>
+      </header>
+      <main>
+        <p>title</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
